e9aa7c985823d87db1a460569b9301ab4f038f48
Lal Verda Çakır <43840298+lalverdacakir@users.noreply.github.com>
2019-08-19 10:07:26 +0300

    Update settings.py

--- mediozon/settings.py
+++ mediozon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'jq6_#&j&4$%19l3t&n%r=3m!wdt0wh%dv=8wxu%4+q_@zee6y3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =  os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
 'agile-beach-58806.herokuapp.com',
